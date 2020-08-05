744e1fbed0453706becd44d0088321b2739bc7ef
drewfidel <62400308+drewfidel@users.noreply.github.com>
2020-08-05 13:13:23 -0700

    finished using filter() and reduce() methods
    finished project

--- Dom-Array_Methods/script.js
+++ Dom-Array_Methods/script.js
@@ -8,12 +8,7 @@ const calculateWealthBtn = document.getElementById('calculate-wealth');
 // People where be stored
 let data = [];
 
-// getRandomUser();
-
-
-
 // Fetch random user and add money 
-
 async function getRandomUser() {
   const res = await fetch('https://randomuser.me/api');
   const data = await res.json();
@@ -48,6 +43,22 @@ function sortByRichest() {
   updateDOM();
 }
 
+// Filter Only Millionaires 
+function showMillionaires() {
+  data = data.filter(user => user.money > 1000000);
+  updateDOM();
+}
+
+// Calculate Total Wealth
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+  main.append(wealthEl);
+}
+
+
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
@@ -79,3 +90,5 @@ function formatMoney(number) {
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
