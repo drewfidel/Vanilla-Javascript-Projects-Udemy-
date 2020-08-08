5acb03c334b4dc0adfa3ed8cc7b8f714237dbdf3
drewfidel <62400308+drewfidel@users.noreply.github.com>
2020-08-08 16:48:47 -0700

    finished hangman project

---
+++ hangman/script.js
@@ -0,0 +1,128 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
+// grabs all figure part classes into node array
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
+
+// Show hidden word
+function displayWord() {
+  wordEl.innerHTML = `
+    ${selectedWord
+      .split('')
+      .map(
+        letter => `
+          <span class="letter">
+            ${correctLetters.includes(letter) ? letter : ''}
+          </span>
+        `
+      )
+      .join('')}
+  `;
+
+  // use regex globally to remove new line 
+  const innerWord = wordEl.innerText.replace(/\n/g, '');
+
+  // If word in word div equals to the actual word
+  // display popup by changing display from none to flex to show up
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = 'Congratulations! You won! 😃';
+    popup.style.display = 'flex';
+  }
+}
+
+// Update the wrong letters element
+// and display figure parts if wrong letter in the array
+// put back parts style display back to none if game restarted
+function updateWrongLettersEl() {
+  wrongLettersEl.innerHTML = `
+  ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+  ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+  `;
+
+  // Display figure parts
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+
+    if (index < errors) {
+      part.style.display = 'block';
+    } else {
+      part.style.display = 'none';
+    }
+  });
+
+  // Check if lost 
+  if (wrongLetters.length == figureParts.length) {
+    finalMessage.innerText = 'Unfortunately you lost. 😕';
+    popup.style.display = 'flex';
+  }
+}
+
+// Show notification
+// Already selected letter notification
+function showNotification() {
+  notification.classList.add('show');
+
+  setTimeout(() => {
+    notification.classList.remove('show');
+  }, 2000);
+}
+
+// Keydown letter press
+window.addEventListener('keydown',
+  e => {
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+      // obtain what key was pressed by user
+      const letter = e.key;
+
+      // if the key letter pressed is in the real word
+      // check first if in correc letters array
+      // if not, add it array, if not, show notification
+      if (selectedWord.includes(letter)) {
+        if (!correctLetters.includes(letter)) {
+          correctLetters.push(letter);
+
+          displayWord();
+        } else {
+          showNotification();
+        }
+      } else {
+        // If user's pressed key is not in wrong letters array
+        // add it to that array then update wrong letters portion
+        // then update wrong letters and hangman figure parts
+        if (!wrongLetters.includes(letter)) {
+          wrongLetters.push(letter);
+
+          updateWrongLettersEl();
+        } else {
+          showNotification();
+        }
+      }
+    }
+  });
+
+// Restart game and play again
+playAgainBtn.addEventListener('click', () => {
+  // Empty arrays
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+  displayWord();
+  updateWrongLettersEl();
+  popup.style.display = 'none';
+})
+
+
+
+displayWord();
