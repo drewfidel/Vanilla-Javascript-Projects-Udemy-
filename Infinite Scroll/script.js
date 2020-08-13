75a7e51207e2f7e76d7f2f5523cb3ff353d3086c
drewfidel <62400308+drewfidel@users.noreply.github.com>
2020-08-13 15:41:19 -0700

    finishedp roject: Infinite Scroll

---
+++ Infinite Scroll/script.js
@@ -0,0 +1,97 @@
+const postsContainer = document.getElementById('posts-container');
+const loading = document.querySelector('.loader');
+const filter = document.getElementById('filter');
+
+let limit = 5;
+let page = 1;
+
+// Fetch posts from API
+async function getPosts() {
+  // get 5 items from data, on whichever page set
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  );
+
+  const data = await res.json();
+  return data;
+}
+
+
+
+// Show posts in DOM
+async function showPosts() {
+  const posts = await getPosts();
+
+  posts.forEach(post => {
+    const postEl = document.createElement('div');
+    postEl.classList.add('post');
+    postEl.innerHTML = `
+      <div class="number">${post.id}</div>
+      <div class="post-info">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+      </div>
+    `;
+
+    postsContainer.appendChild(postEl);
+  });
+}
+
+// Show loading class to show loading circles
+// After 1000, remove 'show' from loading class and 
+// increment page to obtain data from that page 
+function showLoading() {
+  loading.classList.add('show');
+
+  setTimeout(() => {
+    loading.classList.remove('show');
+
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 300);
+  }, 1000);
+}
+
+//Filter posts by input
+function filterPosts(e) {
+  // e is the event of which what was typed in input
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll('.post');
+
+  posts.forEach(post => {
+    const title = post.querySelector('.post-title').innerText.toUpperCase();
+    const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+    // If input is in title or body of post, display posts
+    // If not, display no posts
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = 'flex';
+    } else {
+      post.style.display = 'none';
+    }
+
+  })
+
+}
+
+// Show initial posts 
+showPosts();
+
+
+// Event Listeners
+window.addEventListener('scroll', () => {
+  const {
+    scrollTop,
+    scrollHeight,
+    clientHeight
+  } =
+  document.documentElement;
+
+  // If scrolled down to end of page, show loading circles for a bit 
+  if (scrollTop + clientHeight > scrollHeight - 5) {
+    showLoading();
+  }
+});
+
+filter.addEventListener('input', filterPosts);
