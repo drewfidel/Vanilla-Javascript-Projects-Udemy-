70a7a92fb1bdad5f1abe81dd13384bff3e457016
drewfidel <62400308+drewfidel@users.noreply.github.com>
2020-08-24 15:25:29 -0700

    Project finished: Relaxer app

---
+++ Relaxer App/script.js
@@ -0,0 +1,25 @@
+const container = document.getElementById('container');
+const text = document.getElementById('text');
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2; //3s
+const holdTime = totalTime / 5; //1.5s
+
+breateAnimation();
+
+function breateAnimation() {
+  text.innerText = 'Breathe In!';
+  container.className = 'container grow';
+
+  setTimeout(() => {
+    text.innerText = 'Hold';
+
+    setTimeout(() => {
+      text.innerText = 'Breathe Out!';
+      container.className = 'container shrink';
+    }, holdTime);
+
+  }, breatheTime);
+}
+
+setInterval(breateAnimation, totalTime);
